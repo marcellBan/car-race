01c59e9dd819197552873aad1683cd8a39c5f55a
Bán Marcell <afga93+cc@gmail.com>
2017-08-12 14:52:34 +0200

    add file reader implementation

--- src/com/codecool/BM/carrace/FileLoader.java
+++ src/com/codecool/BM/carrace/FileLoader.java
@@ -1,10 +1,22 @@
 package com.codecool.BM.carrace;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class FileLoader {
     static List<String> loadFile(String fileName) {
-        return new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        try {
+            Scanner data = new Scanner(new File(fileName));
+            while (data.hasNextLine()) {
+                result.add(data.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found.");
+        }
+        return result;
     }
 }
