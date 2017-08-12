f60a56611b7960197dce08bfe52bed6c0f18c12a
Bán Marcell <afga93+cc@gmail.com>
2017-08-12 14:25:46 +0200

    add main functionality

--- src/com/codecool/BM/carrace/Main.java
+++ src/com/codecool/BM/carrace/Main.java
@@ -1,8 +1,6 @@
 package com.codecool.BM.carrace;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     private static final double RAIN_CHANCE = 0.3;
@@ -21,14 +19,35 @@ public class Main {
     }
 
     private static void createVehicles() {
-
+        Car.nameList = FileLoader.loadFile("data/nameList.txt");
+        for (int i = 0; i < VEHICLE_PER_TYPE; i++) {
+            vehicles.add(new Car(rng));
+            vehicles.add(new Motorcycle());
+            vehicles.add(new Truck());
+        }
     }
 
     private static void simulateRace() {
-
+        for (int i = 0; i < RACE_DURATION; i++) {
+            isRaining = rng.nextDouble() < RAIN_CHANCE;
+            Car.setSpeedLimit(rng.nextDouble() < SPEED_LIMIT_CHANCE ? CAR_SPEED_LIMIT : -1);
+            for (Vehicle v : vehicles) {
+                v.moveForAnHour(rng, isRaining);
+            }
+        }
     }
 
     private static void printRaceResults() {
-
+        Collections.sort(vehicles, Comparator.comparingInt(v -> ((Vehicle) v).distanceTravelled).reversed());
+        for (Vehicle v : vehicles) {
+            System.out.println(
+                    v.name +
+                            " travelled " +
+                            v.distanceTravelled +
+                            " kms (" +
+                            v.getClass().getSimpleName() +
+                            ")"
+            );
+        }
     }
 }
